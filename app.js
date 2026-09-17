f9db2a6def3982dd07221192c24391b4f9517d75
RAMSHAD PK <pkramshad28@gmail.com>
2026-09-17 12:37:22 +0530

    Initial CI/CD project

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send(`
+        <h1>CI/CD Pipeline Successfully Deployed!</h1>
+        <p>GitHub Actions + Docker + AWS EC2</p>
+    `);
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
+});
